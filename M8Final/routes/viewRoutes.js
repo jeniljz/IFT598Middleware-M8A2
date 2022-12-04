f693b2006690052b354804809d72cefafa46b878
jeniljz <112844465+jeniljz@users.noreply.github.com>
2022-12-03 22:34:19 -0700

    Add files via upload

---
+++ M8Final/routes/viewRoutes.js
@@ -0,0 +1,24 @@
+//Route file helps routing all the files and calls upon when a requested on web
+
+const express = require('express');
+const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController')
+
+const router = express.Router();
+
+router.get('/', viewsController.getLandingPAge);
+router.get('/loans', viewsController.getAllLoans);
+router.get('/login', viewsController.getLoginForm);
+router.get('/signup', viewsController.getSignInForm);
+router.get('/newLoan', viewsController.createNewLoan);
+router.get('/logout', viewsController.getLandingPAge);
+router.get('/users', viewsController.getUsers);
+
+
+
+router.get('/');
+router.post('/signup',authController.signup);
+router.post('/login',authController.login);
+router.post('/newLoan', authController.newLoan);
+
+module.exports = router;
